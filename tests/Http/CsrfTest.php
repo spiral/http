0d3f8b586245909acab74b415a2bfcdceb9b007e
Wolfy-J <wolfy.jd@gmail.com>
2018-09-03 18:10:37 +0300

    csrf

---
+++ tests/Http/CsrfTest.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Http\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Spiral\Core\Container;
+use Spiral\Http\Configs\HttpConfig;
+use Spiral\Http\HttpCore;
+use Spiral\Http\Middleware\CsrfFirewall;
+use Spiral\Http\Middleware\CsrfMiddleware;
+use Spiral\Http\Pipeline;
+use Zend\Diactoros\ServerRequest;
+
+class CsrfTest extends TestCase
+{
+    private $container;
+
+    public function setUp()
+    {
+        $this->container = new Container();
+        $this->container->bind(HttpConfig::class, new HttpConfig([
+            'basePath'   => '/',
+            'headers'    => [
+                'Content-Type' => 'text/html; charset=UTF-8'
+            ],
+            'middleware' => [],
+            'cookies'    => [
+                'domain'   => '.%s',
+                'method'   => HttpConfig::COOKIE_ENCRYPT,
+                'excluded' => ['PHPSESSID', 'csrf-token']
+            ],
+            'csrf'       => [
+                'cookie'   => 'csrf-token',
+                'length'   => 16,
+                'lifetime' => 86400
+            ]
+        ]));
+    }
+
+    public function testGet()
+    {
+        $core = $this->getCore([CsrfMiddleware::class]);
+        $core->setHandler(function () {
+            return 'all good';
+        });
+
+        $response = $this->get($core, '/');
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('all good', (string)$response->getBody());
+
+        $cookies = $this->fetchCookies($response);
+
+        $this->assertArrayHasKey('csrf-token', $cookies);
+    }
+
+    public function testPostForbidden()
+    {
+        $core = $this->getCore([CsrfMiddleware::class, CsrfFirewall::class]);
+        $core->setHandler(function () {
+            return 'all good';
+        });
+
+        $response = $this->post($core, '/');
+        $this->assertSame(412, $response->getStatusCode());
+    }
+
+    public function testPostOK()
+    {
+        $core = $this->getCore([CsrfMiddleware::class, CsrfFirewall::class]);
+        $core->setHandler(function () {
+            return 'all good';
+        });
+
+        $response = $this->get($core, '/');
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('all good', (string)$response->getBody());
+
+        $cookies = $this->fetchCookies($response);
+
+        $response = $this->post($core, '/', [], [], ['csrf-token' => $cookies['csrf-token']]);
+
+        $this->assertSame(412, $response->getStatusCode());
+
+        $response = $this->post($core, '/', [
+            'csrf-token' => $cookies['csrf-token']
+        ], [], ['csrf-token' => $cookies['csrf-token']]);
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('all good', (string)$response->getBody());
+    }
+
+    protected function getCore(array $middleware = []): HttpCore
+    {
+        return new HttpCore(
+            new HttpConfig([
+                'basePath'   => '/',
+                'headers'    => [
+                    'Content-Type' => 'text/html; charset=UTF-8'
+                ],
+                'middleware' => $middleware,
+                'cookies'    => [
+                    'domain'   => '.%s',
+                    'method'   => HttpConfig::COOKIE_ENCRYPT,
+                    'excluded' => ['PHPSESSID', 'csrf-token']
+                ],
+                'csrf'       => [
+                    'cookie'   => 'csrf-token',
+                    'length'   => 16,
+                    'lifetime' => 86400
+                ]
+            ]),
+            new Pipeline($this->container),
+            $this->container
+        );
+    }
+
+    protected function get(
+        HttpCore $core,
+        $uri,
+        array $query = [],
+        array $headers = [],
+        array $cookies = []
+    ): ResponseInterface {
+        return $core->handle($this->request($uri, 'GET', $query, $headers, $cookies));
+    }
+
+    protected function post(
+        HttpCore $core,
+        $uri,
+        array $data = [],
+        array $headers = [],
+        array $cookies = []
+    ): ResponseInterface {
+        return $core->handle(
+            $this->request($uri, 'POST', [], $headers, $cookies)->withParsedBody($data)
+        );
+    }
+
+    protected function request(
+        $uri,
+        string $method,
+        array $query = [],
+        array $headers = [],
+        array $cookies = []
+    ): ServerRequest {
+        return new ServerRequest(
+            [],
+            [],
+            $uri,
+            $method,
+            'php://input',
+            $headers, $cookies,
+            $query
+        );
+    }
+
+    protected function fetchCookies(ResponseInterface $response)
+    {
+        $result = [];
+
+        foreach ($response->getHeaders() as $line) {
+            $cookie = explode('=', join("", $line));
+            $result[$cookie[0]] = rawurldecode(substr($cookie[1], 0, strpos($cookie[1], ';')));
+        }
+
+        return $result;
+    }
+}
