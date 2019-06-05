33a27638a6067ca52408952be1f917a0126fad4b
Wolfy-J <wolfy.jd@gmail.com>
2019-06-05 18:18:31 +0300

    http version

---
+++ src/Exception/EmitterException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+declare(strict_types=1);
+
+namespace Spiral\Http\Exception;
+
+// todo: license to zend
+class EmitterException extends \RuntimeException
+{
+
+}
